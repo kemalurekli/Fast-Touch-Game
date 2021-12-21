9e1cb8507232e276a1360b863082444a7f177652
kemalurekli <kemalurekli@gmail.com>
2021-12-21 21:32:07 +0300

    splash screen added.

---
+++ app/src/main/java/com/kemalurekli/fasttouch/SplashFragment.kt
@@ -0,0 +1,45 @@
+package com.kemalurekli.fasttouch
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import androidx.navigation.Navigation
+import com.kemalurekli.fasttouch.databinding.FragmentFirstBinding
+import com.kemalurekli.fasttouch.databinding.FragmentSplashBinding
+
+class SplashFragment : Fragment() {
+    private var _binding: FragmentSplashBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSplashBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val backgroundImg : ImageView = binding.splashLogo
+        val sideAnimation = AnimationUtils.loadAnimation(requireContext(),R.anim.slide)
+        backgroundImg.startAnimation(sideAnimation)
+        Handler().postDelayed({
+            Navigation.findNavController(requireView()).navigate(SplashFragmentDirections.actionSplashFragmentToFirstFragment())
+        },3000)
+    }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
